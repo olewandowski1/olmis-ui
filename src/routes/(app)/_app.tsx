e3a431ccaa6f0132e475fb926077970ef092dac0
olewandowski1 <olewandowski@soldevelo.com>
2025-01-27 22:40:22 +0100

    feat: notifications

--- src/routes/(app)/_app.tsx
+++ src/routes/(app)/_app.tsx
@@ -1,3 +1,4 @@
+import { NotificationButton } from '@/components/notification-button';
 import { AppSidebar } from '@/components/sidebar/sidebar';
 import { Button } from '@/components/ui/button';
 import {
@@ -5,8 +6,10 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from '@/components/ui/sidebar';
+import { Typography } from '@/components/ui/typography';
 import { createFileRoute, Outlet } from '@tanstack/react-router';
-import { Bell, LogOut } from 'lucide-react';
+import { LogOut } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
 
 export const Route = createFileRoute('/(app)/_app')({
   component: ProtectedLayout,
@@ -18,6 +21,10 @@ export const Route = createFileRoute('/(app)/_app')({
  * Layout for protected routes. It wraps the content with the sidebar provider.
  */
 function ProtectedLayout() {
+  const { t } = useTranslation('translation', {
+    keyPrefix: 'app.ProtectedLayout',
+  });
+
   return (
     <SidebarProvider>
       <AppSidebar />
@@ -25,13 +32,11 @@ function ProtectedLayout() {
         <header className='flex h-16 shrink-0 items-center gap-4 transition-[width,height] ease-linear border-b group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 px-2'>
           <SidebarTrigger />
 
-          <div className='flex gap-2 ml-auto'>
-            <Button size='icon' variant='outline'>
-              <Bell />
-            </Button>
+          <div className='flex gap-4 ml-auto'>
+            <NotificationButton />
             <Button className='ml-auto' size='sm'>
               <LogOut />
-              <p> Logout </p>
+              <Typography.Small> {t('logout')} </Typography.Small>
             </Button>
           </div>
         </header>
